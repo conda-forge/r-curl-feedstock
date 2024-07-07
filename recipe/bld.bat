5267e4d62d1ddc23ec3b8ea340e8c0deeb00d281
Marcel Bargull <marcel.bargull@udo.edu>
2024-07-08 01:05:26 +0200

    Windows: Try to non-static libcurl
    Signed-off-by: Marcel Bargull <marcel.bargull@udo.edu>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,4 @@
-"%R%" CMD INSTALL --build .
+copy /y configure configure.win || exit 1
+copy /y src\Makevars.in src\Makevars.win || exit 1
+"%R%" CMD INSTALL --configure-vars="INCLUDE_DIR=%LIBRARY_INC:\=/% LIB_DIR=%LIBRARY_LIB:\=/%" --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
