b07f3bbea89ab35ca3d80aa072992415cd998384
Marcel Bargull <marcel.bargull@udo.edu>
2024-07-08 01:52:55 +0200

    Windows: Try configure vars via .Renviron
    Signed-off-by: Marcel Bargull <marcel.bargull@udo.edu>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,8 @@
 copy /y configure configure.win || exit 1
 copy /y src\Makevars.in src\Makevars.win || exit 1
-"%R%" CMD INSTALL --configure-vars="INCLUDE_DIR=%LIBRARY_INC:\=/% LIB_DIR=%LIBRARY_LIB:\=/%" --build .
+(
+echo=INCLUDE_DIR=%LIBRARY_INC:\=/%
+echo=LIB_DIR=%LIBRARY_LIB:\=/%
+) > .Renviron
+"%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
